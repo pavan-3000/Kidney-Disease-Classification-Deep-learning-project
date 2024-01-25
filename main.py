a7b0ea4d28a6d9c5a0a55a5644fd86714ca733be
pavan-3000 <pavansingh3000@gmail.com>
2024-01-25 08:15:23 +0530

    udpated main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.logger import logging
+from src.project.pipeline.ingestion_pipeline import DataIngestionPipeline
+from src.project.logger import logging 
+from src.project.exception import CustomException
+import sys
 
-logging.info("weclime")
+try:
+    logging.info("datas ingestonston stared")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logging.info("data ingestion completd")
+        
+except Exception as e:
+    raise CustomException(e,sys)
+    
